8c7940adf0f3cd25bd895acd84596ff6eeb490ef
Rajchal <rajchalanjal1@gmail.com>
2025-03-04 00:05:26 +0545

    bug fixs

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def get_answers():
 @app.route('/get_chapters',methods=['GET'])
 def get_chapters():
     try:
-        return jsonify({"chapter":"1"})
+        return jsonify({"chapter":"1","question":{student_answers}})
     except Exception as e:
         return jsonify({"error":str(e)}),500
 # Render the webpage to show questions and answers
