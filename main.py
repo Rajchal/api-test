6c1695a7c8eedf9bcefc8d5a334973706344975e
Rajchal <rajchalanjal1@gmail.com>
2025-03-03 22:53:51 +0545

    bug fixs

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def get_answers():
 @app.route('/get_chapters',methods=['GET'])
 def get_chapters():
     try:
-        return "lato sachet"
+        return jsonify({"chapter":"1"})
     except Exception as e:
         return jsonify({"error":str(e)}),500
 # Render the webpage to show questions and answers
